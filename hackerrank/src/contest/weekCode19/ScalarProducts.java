21a88f355533a134c1f1452c17c41ba8e32fd586
akul91 <akulbansal91@gmail.com>
2016-02-22 11:55:06 -0600

    Hackerrank updated contest questions

--- hackerrank/src/contest/weekCode19/ScalarProducts.java
+++ hackerrank/src/contest/weekCode19/ScalarProducts.java
@@ -1,5 +1,9 @@
 package contest.weekCode19;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
@@ -16,21 +20,37 @@ public class ScalarProducts {
 		long first = 0;
 		long second = (long) c;
 		int j = 0;
+		List<Integer> ids = new ArrayList<Integer>();
+		Map<String, String> values = new HashMap<String, String>();
 		for (int i = 2; i < len; i++) {
 			long third = (first + second) % m;
 			if (i % 2 == 0)
 				vec[j][0] = third;
 			else {
 				vec[j][1] = third;
+				String str = vec[j][0] + "," + vec[j][1];
+				if (values.containsKey(str)) {
+					values.put(str, values.get(str) + "," + j);
+				} else {
+					values.put(str, "" + j);
+				}
 				j++;
 			}
 			first = second;
 			second = third;
 		}
+		for (Map.Entry<String, String> entry : values.entrySet()) {
+			String[] parts = entry.getValue().split(",");
+			if (parts.length > 1) {
+				ids.add(Integer.parseInt(parts[0]));
+				ids.add(Integer.parseInt(parts[1]));
+			} else
+				ids.add(Integer.parseInt(parts[0]));
+		}
 		Set<Long> set = new TreeSet<Long>();
-		for (int i = 0; i < n - 1; i++) {
-			for (j = i + 1; j < n; j++) {
-				long sum = vec[i][0] * vec[j][0] + vec[i][1] * vec[j][1];
+		for (int i = 0; i < ids.size() - 1; i++) {
+			for (j = i + 1; j < ids.size(); j++) {
+				long sum = vec[ids.get(i)][0] * vec[ids.get(j)][0] + vec[ids.get(i)][1] * vec[ids.get(j)][1];
 				set.add(sum % m);
 			}
 		}
--- hackerrank/src/contest/weekCode19/TwoRobots.java
+++ hackerrank/src/contest/weekCode19/TwoRobots.java
@@ -1,11 +1,27 @@
 package contest.weekCode19;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TwoRobots {
 
-	static int[][] arr;
-	static int[] min;
+	static int arr[][];
+	static int distance;
+
+	static class Tree {
+		private int r1Pos;
+		private int r2Pos;
+		private int value;
+		private Tree r1;
+		private Tree r2;
+
+		Tree() {
+			r1Pos = -1;
+			r2Pos = -1;
+			value = Integer.MIN_VALUE;
+		}
+	}
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
@@ -13,35 +29,92 @@ public class TwoRobots {
 		for (int i = 0; i < t; i++) {
 			int m = in.nextInt();
 			int n = in.nextInt();
-			int r1 = -1, r2 = -1;
-			int dist = 0;
-			for (int j = 0; j < n; j++) {
-				int s = in.nextInt();
-				int d = in.nextInt();
-				if (r1 == -1) {
-					dist += Math.abs(d - s);
-					r1 = d;
-				} else {
-					if (r1 == d) {
-						dist += Math.abs(r1 - s) + Math.abs(d - s);
-						r1 = d;
-					} else if (r2 == d) {
-						dist += Math.abs(r2 - s) + Math.abs(d - s);
-						r2 = d;
-					} else if (r2 == -1) {
-						dist += Math.abs(d - s);
-						r2 = d;
-					} else if (Math.abs(r1 - s) > Math.abs(r2 - s)) {
-						dist += Math.abs(r2 - s) + Math.abs(d - s);
-						r2 = d;
-					} else {
-						dist += Math.abs(r1 - s) + Math.abs(d - s);
-						r1 = d;
-					}
+			if (n > 1) {
+				arr = new int[n][2];
+				Tree root = makeTree(n);
+				for (int j = 0; j < n; j++) {
+					arr[j][0] = in.nextInt();
+					arr[j][1] = in.nextInt();
 				}
-				// System.out.println(r1 + " " + r2 + " " + dist);
-			}
-			System.out.println(dist);
+				distance = Integer.MAX_VALUE;
+				getMinimum(root, 1, 0);
+			} else
+				distance = Math.abs(in.nextInt() - in.nextInt());
+			System.out.println(distance);
 		}
+		in.close();
+	}
+
+	private static void getMinimum(Tree root, int robot, int i) {
+		if (root.value > distance)
+			return;
+		if (i == 0) {
+			root.value = Math.abs(arr[0][1] - arr[0][0]);
+			root.r1Pos = arr[0][1];
+			root.r1.value = root.value;
+			root.r1.r1Pos = root.r1Pos;
+			root.r2.value = root.value;
+			root.r2.r1Pos = root.r1Pos;
+		} else {
+			if (robot == 1) {
+				root.value += Math.abs(root.r1Pos - arr[i][0]) + Math.abs(arr[i][1] - arr[i][0]);
+				root.r1Pos = arr[i][1];
+				if (root.r1 != null && root.r2 != null) {
+					root.r1.value = root.value;
+					root.r1.r1Pos = root.r1Pos;
+					root.r1.value = root.value;
+					root.r1.r2Pos = root.r2Pos;
+					root.r2.value = root.value;
+					root.r2.r1Pos = root.r1Pos;
+					root.r2.value = root.value;
+					root.r2.r2Pos = root.r2Pos;
+
+				}
+			} else {
+				if (root.r2Pos == -1) {
+					root.value += Math.abs(arr[i][1] - arr[i][0]);
+				} else {
+					root.value += Math.abs(root.r2Pos - arr[i][0]) + Math.abs(arr[i][1] - arr[i][0]);
+				}
+				root.r2Pos = arr[i][1];
+				if (root.r1 != null && root.r2 != null) {
+					root.r1.value = root.value;
+					root.r1.r1Pos = root.r1Pos;
+					root.r1.value = root.value;
+					root.r1.r2Pos = root.r2Pos;
+					root.r2.value = root.value;
+					root.r2.r1Pos = root.r1Pos;
+					root.r2.value = root.value;
+					root.r2.r2Pos = root.r2Pos;
+				}
+			}
+		}
+		if (root.r1 == null && root.r2 == null) {
+			distance = Math.min(distance, root.value);
+			return;
+		}
+		if (root.r1 != null && i + 1 < arr.length)
+			getMinimum(root.r1, 1, i + 1);
+		if (root.r2 != null && i < arr.length)
+			getMinimum(root.r2, 2, i + 1);
+	}
+
+	private static Tree makeTree(int n) {
+		Tree struct = new Tree();
+		List<Tree> list = new ArrayList<Tree>();
+		if (n > 0)
+			list.add(struct);
+		for (int i = 1; i < n; i++) {
+			List<Tree> copy = new ArrayList<Tree>();
+			for (int j = 0; j < list.size(); j++) {
+				Tree temp = list.get(j);
+				temp.r1 = new Tree();
+				copy.add(temp.r1);
+				temp.r2 = new Tree();
+				copy.add(temp.r2);
+			}
+			list = copy;
+		}
+		return struct;
 	}
 }
